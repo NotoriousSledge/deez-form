f0162f17e90ea4b9fd0a3178475e1266963cc43c
Exsjabe <jakob.bergstrom@explizit.se>
2023-10-05 09:15:50 +0200

    Got some form of building going on

---
+++ prettier.config.cjs
@@ -0,0 +1,12 @@
+// @ts-check
+
+/** @type{import('prettier').Config} */
+const config = {
+    semi: true,
+    trailingComma: 'es5',
+    tabWidth: 4,
+    singleQuote: true,
+    printWidth: 80,
+};
+
+module.exports = config;
